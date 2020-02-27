4957ad12cdc39833bddd94ae88db4a3948de7312
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-02-27 13:00:59 +0100

    basic set up

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from "../frontend/src/react";
+import ReactDOM from "../frontend/src/react-dom";
+import "./index.css";
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
