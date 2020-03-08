58c9bfc5a65dc41244c411e7b68158bdb35ca50f
Johanna Luks Wærn <johanna@misswaern.se>
2020-03-08 15:20:13 +0100

    update landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -14,6 +14,9 @@ export const Landing = () => {
       <Button onClick={() => history.push('/login')}>Login</Button>
       <Button onClick={() => history.push('/signup')}>Sign up</Button>
       <h2>Info about app</h2>
+      <p>First fact</p>
+      <p>Second fact</p>
+      <p>Third fact</p>
     </div>
   )
 }
