a3b6d5f5773270d4ad7ed84e97587a9333d24e0f
horsefacegirl <lindstrmida@gmail.com>
2020-03-12 17:41:34 +0100

    fetch levels and render in calender
    - temporary styling

--- frontend/src/components/Statistics.js
+++ frontend/src/components/Statistics.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavBar } from './NavBar'
 import Calendar from 'react-calendar'
 
 export const Statistics = () => {
+  const [levels, setLevels] = useState()
+  const handleSetLevels = () => {
+    //Don't run code if we don't have levels
+    if (!levels) return
+    levels.forEach(level => {
+      const date = new Date(level.date)
+      const formatDate = date.toLocaleDateString("sv-SE", { year: "numeric", month: "long", day: "numeric" })
+      //Select date by aria-label (unique)
+      const selectedDate = document.querySelector(`[aria-label="${formatDate}"]`)
+      selectedDate.parentNode.classList.add('styledDate')
+    })
+  }
+  //Triggers when levels change (useState)
+  useEffect(() => {
+    handleSetLevels()
+  }, [levels])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/levels", {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': window.localStorage.getItem('accessToken')
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        //Saving levels in state
+        setLevels(json)
+      })
+  }, [])
+
   return (
     <div>
       <p>Statistics</p>
