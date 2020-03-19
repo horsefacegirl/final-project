d5dc06b116a2b621ddd4a35183260fdcf9e31ab9
Johanna Luks Wærn <johanna@misswaern.se>
2020-03-19 16:07:41 +0100

    make sure tips works when there is no level set

--- src/components/Tips.js
+++ src/components/Tips.js
@@ -6,25 +6,31 @@ import { FlexColumn, H1, Button } from 'styles'
 export const Tips = () => {
   const history = useHistory()
   const accessToken = window.localStorage.getItem('accessToken')
-  const levelValue = window.localStorage.getItem('levelValue')
-  const getRandomTips =
-    tips[`tips${levelValue}`][
-    Math.floor(Math.random() * tips[`tips${levelValue}`].length)
-    ]
 
   if (!accessToken) {
     history.push('/')
     return null
   }
 
+  const levelValue = window.localStorage.getItem('levelValue')
+
+  if (!levelValue) {
+    history.push('/home')
+    return null
+  }
+
+  const getRandomTips =
+    tips[`tips${levelValue}`][
+      Math.floor(Math.random() * tips[`tips${levelValue}`].length)
+    ]
+
   return (
     <FlexColumn>
       <H1>Todays energy level is {levelValue}%</H1>
       <p>
         Always listen to your body and do what feels good for you. But if your
-        mind is blank; here is a tip that we think matches your level of
-        energy.
-        </p>
+        mind is blank; here is a tip that we think matches your level of energy.
+      </p>
       <h2>{getRandomTips}</h2>
       <Button onClick={() => history.push('/stats')}>See your pattern</Button>
     </FlexColumn>
